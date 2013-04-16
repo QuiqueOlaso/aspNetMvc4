fc579e9022258f8c740229822b2e0a1c0cf6d617
Quique Olaso <qolaso@hotmail.com>
2013-04-16 18:46:40 +0200

    2013/04/16 18:45h : picnic time! I've finished tutorial --> http://www.asp.net/mvc/tutorials/mvc-4/getting-started-with-aspnet-mvc4/adding-a-controller

---
+++ aspNetMvc4/Controllers/HelloWorldController.vb
@@ -0,0 +1,43 @@
+﻿Namespace aspNetMvc4
+    Public Class HelloWorldController
+        Inherits System.Web.Mvc.Controller
+
+        '
+        ' GET: /HelloWorld
+
+        '**************** INDEX *****************
+
+        'Function Index() 'As ActionResult
+        ''http://www.asp.net/mvc/tutorials/mvc-4/getting-started-with-aspnet-mvc4/adding-a-controller
+        ''forma más sencilla de hacerlo
+        ''comentando as actionresult en la definición de función
+        ''se visualiza con http://localhost:1038/HelloWorld
+        ''o ''se visualiza con http://localhost:1038/HelloWorld/Index
+        'Return "This is my <b>Default</b> Action..."
+        'End Function
+
+        Function Index() As ActionResult
+            ViewData("Message") = "This is my <b>Default</b> Action..."
+            Return View()
+        End Function
+
+
+        '**************** WELCOME *****************
+
+        'Function Welcome() 'As ActionResult
+        'http://www.asp.net/mvc/tutorials/mvc-4/getting-started-with-aspnet-mvc4/adding-a-controller
+        ''forma más sencilla de hacerlo
+        ''comentando as actionresult en la definición de función
+        ''se visualiza con http://localhost:1038/HelloWorld/Welcome
+        'Return "This is the Welcome Action Method..."
+        'End Function
+
+        Function Welcome(ByVal name As String, ByVal numTimes As String) As ActionResult
+            Dim theTextReturn As String
+            theTextReturn = HttpUtility.HtmlEncode("This is the Welcome Action Method...: hello " + name + ", NumTimes is: " + numTimes)
+            ViewData("Message") = theTextReturn
+            Return View()
+        End Function
+
+    End Class
+End Namespace
---
+++ aspNetMvc4/Views/HelloWorld/Welcome.vbhtml
@@ -0,0 +1,15 @@
+﻿@Code
+    'lo comentamos porque lo pasamos al HomeController
+    'ViewData("Title") = "Página principal"
+End Code
+
+@section featured
+    <section class="featured">
+        <div class="content-wrapper">
+            <hgroup class="title">
+                <h1>@ViewData("Message")</h1>
+            </hgroup>            
+        </div>
+    </section>
+End Section
+
