e7aa8697b7319bc277815234f092c6ec25744ca7
Quique Olaso <qolaso@hotmail.com>
2013-04-16 17:23:29 +0200

    2013/04/16 17:23h --> Blank Creation

---
+++ aspNetMvc4/Controllers/HomeController.vb
@@ -0,0 +1,21 @@
+﻿Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    Function Index() As ActionResult
+        ViewData("Message") = "Modifique esta plantilla para poner en marcha su aplicación ASP.NET MVC."
+
+        Return View()
+    End Function
+
+    Function About() As ActionResult
+        ViewData("Message") = "Página de descripción de la aplicación."
+
+        Return View()
+    End Function
+
+    Function Contact() As ActionResult
+        ViewData("Message") = "Página de contacto."
+
+        Return View()
+    End Function
+End Class
